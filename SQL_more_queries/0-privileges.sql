9b884e1c21d93c7ef0786cb9cbeb3b7f5fd56166
daisykitio <daisycheptoek6788@gmail.com>
2023-08-18 13:37:27 -0700

    My privileges!

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,7 @@
+-- 0-privileges.sql
+CREATE USER 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+CREATE USER 'user_0d_2'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
+
