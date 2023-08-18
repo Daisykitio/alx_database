73f7da9289b20f0141ec116c72624d993707c67f
daisykitio <daisycheptoek6788@gmail.com>
2023-08-18 14:01:41 -0700

    Unique ID

---
+++ SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,10 @@
+-- 5-unique_id.sql
+-- Create the table unique_id if it doesn't exist
+
+-- Create the table if it doesn't exist
+CREATE TABLE IF NOT EXISTS unique_id (
+    id INT DEFAULT 1 UNIQUE,
+    name VARCHAR(256),
+    PRIMARY KEY (id)
+);
+
